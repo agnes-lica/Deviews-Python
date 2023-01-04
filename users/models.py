a47556684cf50b4636af0b57f94cba2661c17fbf
Thiago <thiagocezar32@gmail.com>
2023-01-04 12:07:30 -0300

    :rocket: criando models

--- users/models.py
+++ users/models.py
@@ -1,6 +1,11 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    ...
+    name = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+    bio = models.TextField(max_length=255)
+    profile_picture = models.CharField()
+    is_active = models.BooleanField(default=True)
