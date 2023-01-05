99db108bd0592b92c0c47e825e38acd3eee63605
pedrolibas <pedrogv03@hotmail.com>
2023-01-05 18:54:27 -0300

    :rocket: finished fires  post route

--- deviews/settings.py
+++ deviews/settings.py
@@ -91,31 +91,31 @@ TEMPLATES = [
     },
 ]
 
+WSGI_APPLICATION = "deviews.wsgi.application"
+
 # codigo para teste em sqlite3
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-WSGI_APPLICATION = "deviews.wsgi.application"
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("POSTGRES_DB"),
-#         "USER": os.getenv("POSTGRES_USERNAME"),
-#         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USERNAME"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
