f14811e37949c9b88babaea01aa255cc232d4aa0
emimelo <emillymelo3@gmail.com>
2023-01-05 16:59:20 -0300

    :construction: Create models post

--- posts/models.py
+++ posts/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 class Post(models.Model):
-    ...
+    content = models.TextField()
+    post_picture = models.URLField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = ...
+    techs = ...
