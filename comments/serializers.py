ed0ea30c24b91fbf81aae622e101b10194976734
Agnes-lica <agnes.lica@gmail.com>
2023-01-05 14:05:56 -0400

    :rocket: Criando serializers, views, urls e permissão de adm

--- comments/serializers.py
+++ comments/serializers.py
@@ -5,7 +5,19 @@ from .models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
-        ...
+        model = Comment
+        fields = ["id", "content", "created_at",
+                  "updated_at", "user_id", "post_id"]
+
+        read_only_fields = ["id", "user_id",
+                            "post_id", "created_at", "updated_at"]
 
     def create(self, validated_data: dict) -> Comment:
-        ...
+        return Comment.objects.create(**validated_data)
+
+    def update(self, instance: Comment, validated_data: dict) -> Comment:
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+
+        instance.save()
+        return instance
