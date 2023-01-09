ccb1cd2d064e18ddb6cda5ae2a49f6e2c30cfd43
Agnes-lica <agnes.lica@gmail.com>
2023-01-09 19:28:06 -0400

    :rocket: preparando para o deploy

--- deviews/urls.py
+++ deviews/urls.py
@@ -20,6 +20,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("fires.urls")),
+    path('api/', include("comments.urls")),
+    
     # Documents
     path('api/docs/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
