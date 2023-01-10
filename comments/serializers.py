3c5eb622e74a9597af30880a83c60c7a38eda1aa
Thiago <thiagocezar32@gmail.com>
2023-01-10 15:22:11 -0300

    :bug: corrigindo bugs views

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
+
 from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ["id", "content", "created_at",
-                  "updated_at", "user_id", "post_id"]
+        fields = ["id", "content", "created_at", "updated_at", "user_id", "post_id"]
 
-        read_only_fields = ["id", "user_id",
-                            "post_id", "created_at", "updated_at"]
+        read_only_fields = ["id", "user_id", "post_id", "created_at", "updated_at"]
 
     def create(self, validated_data: dict) -> Comment:
         return Comment.objects.create(**validated_data)
