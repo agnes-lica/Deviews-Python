a574a67277e67b0c320f94e9d2d2c35ab8fdc070
anavgbc <agomesbc@gmail.com>
2023-01-09 23:52:33 -0300

    :construction: posts and users relations

--- posts/models.py
+++ posts/models.py
@@ -9,5 +9,5 @@ class Post(models.Model):
     post_picture = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="posts", default=1)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="posts")
     techs = models.ManyToManyField("techs.Tech", related_name="posts")
