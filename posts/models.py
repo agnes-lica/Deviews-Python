6bd1dd05d233c4eccfce3178b0ac8734cc566f26
pedrolibas <pedrogv03@hotmail.com>
2023-01-10 15:21:37 -0300

    :bug: fix bug create posts

--- posts/models.py
+++ posts/models.py
@@ -10,4 +10,4 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="posts")
-    techs = models.ManyToManyField("techs.Tech", related_name="posts")
+    tech = models.ForeignKey("techs.Tech", on_delete=models.CASCADE, related_name="posts")
--- posts/serializers.py
+++ posts/serializers.py
@@ -5,8 +5,8 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ["id", "content", "post_picture", "created_at", "updated_at", "user_id", "techs", "fires"]
-        read_only_fields = ["created_at", "updated_at", "user_id", "techs", "fires"]
+        fields = ["id", "content", "post_picture", "created_at", "updated_at", "user_id", "tech", "fires"]
+        read_only_fields = ["created_at", "updated_at", "user_id", "fires"]
 
     def create(self, validated_data: dict) -> Post:
         return Post.objects.create(**validated_data)
